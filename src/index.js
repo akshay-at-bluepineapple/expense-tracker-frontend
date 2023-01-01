8547fb87bf6c120596263caf9ee94fc72735482e
akshay-at-bluepineapple <akshay.vishwakarma@bluepineapple.io>
2023-01-01 23:13:16 +0530

    add user reducer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+//redux 
 import { Provider } from "react-redux";
 import store from "./redux/store/store";
 import App from "./App";
@@ -17,6 +18,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import usersReducer from "../slices/users/usersSlices"
 
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        users:usersReducer,
+    },
   });
 
   export default store;
